002188c86d65344ce397fdd686255ad2944202c6
Pavithran G <119714223+G-Pavithran-dev@users.noreply.github.com>
2023-12-24 04:39:55 +0000

    app commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ImageToogler from './components/image_toogler';
+
+function App() {
+  return (
+    <>
+      <ImageToogler />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/image_toogler.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Box, Typography,  } from "@mui/material";
+
+const ImageToogler = () => {
+    <Box>
+        <Typography variant="h1" sx={{fontFamily:"cursive"}}>Imagify</Typography>
+    </Box>
+}
+
+export default ImageToogler;
